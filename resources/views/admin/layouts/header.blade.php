6656e9bd84226051d728d408f5e19aa1486cd657
ankurvaghasiya21 <ankurvaghasiya21@gmail.com>
2022-02-09 16:52:33 +0530

    remove image

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
-      <a class="navbar-brand brand-logo me-5" href="index.html"><img src="{{ asset('template/images/favicon_logo.png') }}" class="me-2" alt="logo"/></a>
-      <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('template/images/impulsion_seo.png') }}" alt="logo"/></a>
+      <a class="navbar-brand brand-logo me-5" href="{{ route('admin.dashboard') }}"><img src="{{ asset('template/images/favicon_logo.png') }}" class="me-2" alt="logo"/></a>
+      <a class="navbar-brand brand-logo-mini" href="{{ route('admin.dashboard') }}"><img src="{{ asset('template/images/impulsion_seo.png') }}" alt="logo"/></a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
       <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -70,7 +70,7 @@
         </li>
         <li class="nav-item nav-profile dropdown">
           <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-            <img src="{{ asset('template/images/faces/face28.jpg') }}" alt="profile"/>
+            <i class="icon-ellipsis"></i>
           </a>
           <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
             <a class="dropdown-item">
@@ -89,11 +89,6 @@
                 @csrf
             </form>
           </div>
-        </li>
-        <li class="nav-item nav-settings d-none d-lg-flex">
-          <a class="nav-link" href="#">
-            <i class="icon-ellipsis"></i>
-          </a>
         </li>
       </ul>
       <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
