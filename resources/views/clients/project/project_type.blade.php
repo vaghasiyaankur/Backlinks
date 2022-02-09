26a5b6018a34949a43bc51d23c6f599e153e118c
vaghasiyaankur <ankurvaghasiya21@gmail.com>
2022-02-09 10:14:03 +0530

    merge code

--- resources/views/clients/project/project_type.blade.php
+++ resources/views/clients/project/project_type.blade.php
@@ -10,21 +10,8 @@
                         </div>
                         <div class="row pt-5">
                             @forelse ($data_project as $project_data)
-<<<<<<< HEAD
                                 <div class="col-3 mb-5">
                                     <div class="single-audit">
-                                        <a href="{{ url('template/images/uploads/'.$project_data->project_file) }}" __target="blank" class="text-decoration-none d-inline-block">
-                                            <div class="audit-image">
-                                                <img src="{{ asset('template/images/uploads/'.$project_data->project_file) }}" alt="" width="300px">
-                                            </div>
-                                            <div class="image-info position-relative">
-                                                <button class="btn btn-primary mt-2">{{ $project_data->project_file }}</button>
-                                                <img src="{{ asset('template/images/icons/audit-image.png')}}" alt="">
-                                            </div>
-                                        </a>
-                                    </div>
-=======
-                                <div class="col-4">
                                     <a href="{{ url('template/images/uploads/'.$project_data->project_file) }}" __target="blank" class="text-decoration-none">
                                         @php
                                             $allowed = array('gif', 'png', 'jpg','jpeg');
@@ -40,27 +27,19 @@
                                             <button class="btn btn-primary mt-2">{{ $project_data->project_file }}</button>
                                         </div>
                                     </a>
->>>>>>> a50fa7fcab6b730d310c36ba8ecc360fa0f3f986
+                                    </div>
                                 </div>
                             @empty
                                 <div class="text-center">No Data Found!!</div>
                             @endforelse
                         </div>
-<<<<<<< HEAD
                         <div class="row mt-3 justify-content-center">
                             <div class="col-lg-12 text-center mb-3">
                                 <div class="d-inline-block">
                                     @for ($i = 1; $i <= $datamonths->months; $i++)
-                                        <a href="{{ route('client.project.type', [$id,$type,$i])}}" class="btn btn-primary">{{$i}}</a>
+                                     <a href="{{ route('client.project.type', [$id,$i,$type])}}" class="btn btn-primary">{{$i}}</a>
                                     @endfor
                                 </div>
-=======
-                        <div class="row mt-3">
-                            <div class="col-lg-12 float-start">
-                                @for ($i = 1; $i <= $datamonths->months; $i++)
-                                    <a href="{{ route('client.project.type', [$id,$i,$type])}}" class="btn btn-primary">{{$i}}</a>
-                                @endfor
->>>>>>> a50fa7fcab6b730d310c36ba8ecc360fa0f3f986
                             </div>
                         </div>
                     </div>
